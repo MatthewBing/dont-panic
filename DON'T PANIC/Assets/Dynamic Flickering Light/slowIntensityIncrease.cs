3640ab92b3a92b1f781911b85e1a14eccf1c6cc9
Matt Bing <Matthew.h.bing@gmail.com>
2016-11-29 22:35:31 -0700

    Scientist Voice Line + Fade in Improved
    man this sounds weird. NEEDS REDOING!

---
+++ DON'T PANIC/Assets/Dynamic Flickering Light/slowIntensityIncrease.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class slowIntensityIncrease : MonoBehaviour {
+
+    new public Light light = null;
+    private float EndTime = 5;
+    private float StartTime = 0;
+
+    IEnumerator slowLightTurnon(float time)
+    {
+        yield return new WaitForSeconds(time);
+        light.intensity = 0;
+        light.enabled = true;
+    }
+
+    void Start () {
+        light = GetComponent<Light>();
+        light.enabled = false;
+        StartCoroutine(slowLightTurnon(1f));
+    }
+	
+	void Update () {
+        if (light && Time.time < 10)
+            light.intensity = Time.time/5;
+    }
+}
